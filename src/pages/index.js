6c6da09d6b1515d15556af054f05be2aae16cb70
Alanna Celentano <celentanoad91@gmail.com>
2020-07-23 18:44:51 -0400

    deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import UserPage from "./UserPage";
 import { Grommet, Box, Button } from "grommet";
 import theme from "../portfolioTheme";
-import Layout from "../components/Layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 function IndexPage() {
   const [user, setUser] = useState(null);
