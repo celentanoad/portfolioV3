461f8892f49203c55957c243e3ca2b4ec782bc61
Alanna Celentano <celentanoad91@gmail.com>
2020-07-24 10:02:31 -0400

    update meta property

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,6 +67,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          property: `og:image`,
+          content: "https://i.imgur.com/tU6UT9u.png"
+        }
       ].concat(meta)}
     />
   )
